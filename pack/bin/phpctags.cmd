6590b88a3f86e75f5e5ffc77dd7eb953768d89f3
cuiliang <cuiliang@live.cn>
2017-02-04 18:18:56 +0800

    delete plugins

--- pack/bin/phpctags.cmd
+++
@@ -1,5 +0,0 @@
-@rem Do not use "echo off" to not affect any child calls.
-@setlocal
-@if not exist "%PORTABLE_APPS%" @set PORTABLE_APPS=%HOME%\Apps
-
-@php %HOME%\vimfiles\pack\plugin\start\phpctags\bin\phpctags %*
